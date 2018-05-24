5a2261e3a5791bb47ae69ea034fd99392b162c36
Rafael Cauduro Oliveira Macedo <rafael_cauduro@hotmail.com>
2018-05-24 15:15:12 -0300

    Bomb Mechanics Start
    Sprites Setup + Bomb Abstract Script

---
+++ 8bit Classic Game/Assets/Scripts/Bomb.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Bomb
+{
+    //Generic Bomb Variables
+    int radius;
+
+    //Explode Method for Bomb (vary according to Bomb Type)
+    public abstract void explode();
+}
