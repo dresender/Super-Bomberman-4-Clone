ac4ad1105cfbe8a742ea23afc4c0d8e6c62af0b6
DESKTOP-KP5Q71B\danie <danielcelta2000@yahoo.com.br>
2018-05-27 13:33:32 -0300

    Initial Pathfinding Setup
    Grid and placeholders placed. Initial manual collisions detection. No pathfinding yet.

---
+++ 8bit Classic Game/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour 
+{
+
+	public float initialSpeed = 0.5f;
+
+	private Vector2 movement;
+	private Vector2 initialPosition;
+	private float speed;
+	private enum collisionType { left, right, up, down, free };
+	private collisionType collision;
+
+	void Start () 
+	{
+		initialPosition = new Vector2(-0.961f, 0.722f);
+		movement = new Vector2();
+		movement = initialPosition;
+		speed = initialSpeed;
+	}
+
+	void Update () 
+	{
+		DetectInput();
+	}
+
+	private void DetectInput()
+	{
+		if (Input.GetKey("up"))
+		{
+			movement +=  Vector2.up * speed * Time.deltaTime; 
+		}
+		if (Input.GetKey("down"))
+		{
+			movement +=  Vector2.down * speed * Time.deltaTime; 
+		}
+		if (Input.GetKey("right"))
+		{
+            if (collision != collisionType.right)
+			    movement +=  Vector2.right * speed * Time.deltaTime; 
+		}
+		if (Input.GetKey("left"))
+		{
+           //if (collision != collisionType.left)
+                movement +=  Vector2.left * speed * Time.deltaTime; 
+		}
+
+		Move();
+	}
+
+	private void Move()
+	{
+		transform.position = movement;
+	}
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.transform.position.x > transform.position.x)
+        {
+            collision = collisionType.right;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        collision = collisionType.free;
+    }
+
+}
