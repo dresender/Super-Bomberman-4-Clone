23543c3f7835b9e1e3c2289145a9dad7ec9324c7
Daniel de Resende <danielcelta2000@yahoo.com.br>
2018-06-15 22:45:27 -0300

    Input Attempt

--- 8bit Classic Game/Assets/Scripts/Scene Test Scripts/Input + Pathfinding/PlayerInput.cs
+++ 8bit Classic Game/Assets/Scripts/Scene Test Scripts/Input + Pathfinding/PlayerInput.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerInput : MonoBehaviour 
+public enum Direction { up, down, left, right, none };
+
+public class PlayerInput : MonoBehaviour
 {
     //Player Movement Speed
-	public float speed = 125f;
+    public float speed = 125f;
+
+    public float raycastMargin;
 
     //Collider Size
     public Vector2 colliderSize;
@@ -14,6 +18,7 @@ public class PlayerInput : MonoBehaviour
     void Start()
     {
         colliderSize = GetComponent<BoxCollider2D>().size;
+        raycastMargin = GetComponent<BoxCollider2D>().size.x / 2f;
     }
 
     //Update Method
@@ -25,6 +30,8 @@ public class PlayerInput : MonoBehaviour
     //Movement Algorithm
 	private void MovePlayer()
 	{
+        Direction dirMovement = Direction.none;
+
         //Initialize Movement Vector
         Vector2 movement = transform.position;
 
@@ -34,21 +41,25 @@ public class PlayerInput : MonoBehaviour
 		{
             hasMoved = true;
             movement += Vector2.up * speed * Time.deltaTime;
+            dirMovement = Direction.up;
 		}
 		else if(Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
 		{
             hasMoved = true;
             movement += Vector2.down * speed * Time.deltaTime;
+            dirMovement = Direction.down;
         }
 		else if(Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
 		{
             hasMoved = true;
             movement += Vector2.right * speed * Time.deltaTime;
+            dirMovement = Direction.right;
         }
 		else if(Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
 		{
             hasMoved = true;
             movement += Vector2.left * speed * Time.deltaTime;
+            dirMovement = Direction.left;
         }
 
         //Check for Collisions
@@ -61,10 +72,51 @@ public class PlayerInput : MonoBehaviour
             //Will Always Collide with Itself!
             if (collisions.Length > 1)
             {
-                for(int i = 1; i < collisions.Length; i++)
+                for(int i = 0; i < collisions.Length; i++)
                 {
                     //TODO: Add more logic here. Certain powerups can modify this.
-                    if(collisions[i].tag == "Destroyable" || collisions[i].tag == "Indestructible") canMove = false;
+                    if (collisions[i].tag == "Destroyable" || collisions[i].tag == "Indestructible")
+                    {
+                        if (dirMovement == Direction.up)
+                        {
+                            RaycastHit2D hitMiddle = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y + raycastMargin + 0.01f), Vector2.up);
+                            RaycastHit2D hitRight = Physics2D.Raycast(new Vector2(transform.position.x + raycastMargin, transform.position.y + raycastMargin + 0.01f), Vector2.up);
+                            RaycastHit2D hitLeft = Physics2D.Raycast(new Vector2(transform.position.x - raycastMargin, transform.position.y + raycastMargin + 0.01f), Vector2.up);
+
+                            if (hitRight.distance == hitMiddle.distance)
+                            {
+                                movement.x -= 0.1f;
+                            }
+                            else if (hitLeft.distance == hitMiddle.distance)
+                            {
+                                movement.x += 0.1f;
+                            }
+                            else
+                            {
+                                canMove = false;
+                            }
+                        }
+                        else if(dirMovement == Direction.down)
+                        {
+                            RaycastHit2D hitMiddle = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y - raycastMargin - 0.01f), Vector2.down);
+                            RaycastHit2D hitRight = Physics2D.Raycast(new Vector2(transform.position.x + raycastMargin, transform.position.y - raycastMargin - 0.01f), Vector2.down);
+                            RaycastHit2D hitLeft = Physics2D.Raycast(new Vector2(transform.position.x - raycastMargin, transform.position.y - raycastMargin - 0.01f), Vector2.down);
+
+                            if (hitRight.distance == hitMiddle.distance)
+                            {
+                                movement.x += 0.1f;
+                            }
+                            else if (hitLeft.distance == hitMiddle.distance)
+                            {
+                                movement.x -= 0.1f;
+                            }
+                            else
+                            {
+                                canMove = false;
+                            }
+                        }
+                        else canMove = false;
+                    }
                 }
             }
 
