df380dab50567a72d53674b867daac8894139c30
Daniel de Resende <danielcelta2000@yahoo.com.br>
2018-07-14 16:20:52 -0300

    Music Manager Implemented

---
+++ 8bit Classic Game/Assets/Scripts/Controllers/AudioManager.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+using System;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+
+    private string currentSceneName;
+
+    //Singleton Instance Variable
+    private static AudioManager instance;
+    public static AudioManager Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+
+    //Setting up the sounds
+    void Awake ()
+    {
+        if (Instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+
+        //Building library
+        foreach (Sound s in sounds)
+        {
+            //For each sound we are looking for, add the audiosource component
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+	}
+
+    //Simple method to play musics that are managed by the script Manager
+    public void Play(string name)
+    {
+        //Find the sound in the sounds array, where sound.name is equal to the passed name
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+
+        //Play the sound source stored above
+        s.source.Play();
+    }
+
+    public void StopPlaying(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        s.source.Stop();
+    }        
+}
---
+++ 8bit Classic Game/Assets/Scripts/Sounds/PlayMusics.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+public class PlayMusics: MonoBehaviour
+{
+    public string loadedScene;
+
+    private AudioManager aManager;
+
+	void Start ()
+    {
+        loadedScene = "Main Menu";
+        aManager = FindObjectOfType<AudioManager>();
+
+        PlaySceneMusic();
+	}
+
+    public void ChangeMusicAccordingToActiveScene()
+    {
+        StopPlayingCurrentMusic();
+        PlaySceneMusic();
+    }
+
+    private void PlaySceneMusic()
+    {
+        switch (loadedScene)
+        {
+            case "Main Menu":
+                PlayMainMenuThemeSong();
+                break;
+            case "World One":
+                PlayWorldOneThemeSong();
+                break;
+            case "Game Over":
+                PlayGameOverThemeSong();
+                    break;
+        }
+    }
+	
+	void PlayMainMenuThemeSong ()
+    { 
+        aManager.Play("Main Menu");
+	}
+
+    void PlayWorldOneThemeSong()
+    {
+        aManager.Play("World One");
+    }
+
+    void PlayGameOverThemeSong()
+    {
+        aManager.Play("Game Over");
+    }
+
+    void StopPlayingCurrentMusic()
+    {
+        aManager.StopPlaying(loadedScene);
+    }
+}
