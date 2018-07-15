284241aede2add8dfa5fb1995015be8cf12c86ff
Rafael Cauduro Oliveira Macedo <rafael_cauduro@hotmail.com>
2018-07-15 01:49:01 -0300

    Fixed Victory Animation + Bridge Animation

---
+++ 8bit Classic Game/Assets/Scripts/TilesAndItens/Bridge.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bridge : MonoBehaviour
+{
+    //References
+    public SpriteRenderer tile1;
+    public SpriteRenderer tile2;
+    public SpriteRenderer tile3;
+    public SpriteRenderer tile4;
+
+    public Sprite tile1normal;
+    public Sprite tile1withPlayer;
+    public Sprite tile2normal;
+    public Sprite tile2withPlayer;
+    public Sprite tile3normal;
+    public Sprite tile3withPlayer;
+    public Sprite tile4normal;
+    public Sprite tile4withPlayer;
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        tile1.sprite = tile1normal;
+        tile2.sprite = tile2normal;
+        tile3.sprite = tile3normal;
+        tile4.sprite = tile4normal;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        tile1.sprite = tile1withPlayer;
+        tile2.sprite = tile2withPlayer;
+        tile3.sprite = tile3withPlayer;
+        tile4.sprite = tile4withPlayer;
+    }
+}
